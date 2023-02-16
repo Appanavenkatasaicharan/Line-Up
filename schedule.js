0fcaebbbdfde182d7e7c8707995da37cf865053a
Appanavenkatasaicharan <109053463+Appanavenkatasaicharan@users.noreply.github.com>
2023-02-16 22:57:08 +0530

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -15,3 +15,55 @@ function displayDate(){
     seconds.innerHTML = d.getSeconds().toString();
     setTimeout(displayDate,1000);
 }
+function addTask()
+{
+var task = document.createElement("div");
+task.className = "task";
+var temp = document.createElement("div");
+temp.className = "temp";
+var text = document.createElement("div");
+text.className = "text";
+var time = document.createElement("div");
+time.className = "time";
+var remover = document.createElement("input");
+remover.type = "checkbox";
+remover.className = "remover";
+remover.addEventListener('click',deleteTask.bind(null,remover))
+var t1 = document.getElementById("task-input");
+var c = document.getElementsByClassName("wrapper");
+var t2 = document.getElementById("time-input");
+if(t1.value==''){
+return;
+}
+if(t2.value==''){
+return;
+}
+text.innerHTML = t1.value;
+t1.value = '';
+var h = parseInt(t2.value.slice(0,2));
+var m = parseInt(t2.value.slice(3,5));
+if(h>11){
+if(h==12){
+time.innerHTML = h.toString() + ':' +m.toString() + ' PM';
+}
+else{
+time.innerHTML = (h-12).toString() +':'+ m.toString() + ' PM';
+}
+24
+}
+else{
+time.innerHTML = h.toString() + ':' +m.toString() + ' AM';
+}
+t2.value='';
+temp.appendChild(remover);
+temp.appendChild(text);
+task.appendChild(temp);
+task.appendChild(time);
+c[0].appendChild(task);
+}
+function deleteTask(child)
+{
+var par = child.parentNode.parentNode;
+var t = document.getElementsByClassName("wrapper");
+t[0].removeChild(par);
+}
